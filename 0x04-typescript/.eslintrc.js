527dc5ff9714811918d1109639e69ea16492f0b8
Shyaka Ndamukunda <shyakandamukunda@gmail.com>
2025-07-14 13:47:05 +0200

    Update .eslintrc.js

--- 0x04-typescript/.eslintrc.js
+++ 0x04-typescript/.eslintrc.js
@@ -1,1 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended', // recommended rules
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
 
